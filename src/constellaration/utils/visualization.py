7f79b0045b51977972822e0c64611860f0764593
Maria Pascu <mariap@proximafusion.com>
2025-05-02 14:57:17 +0200

    Plot surfaces

--- src/constellaration/utils/visualization.py
+++ src/constellaration/utils/visualization.py
@@ -3,9 +3,68 @@ import pathlib
 import booz_xform
 import matplotlib.figure as mpl_figure
 import matplotlib.pyplot as plt
+import numpy as np
+from plotly import graph_objects as go
 from constellaration.boozer import boozer as boozer_module
+from constellaration.geometry import surface_rz_fourier, surface_utils
 from constellaration.mhd import vmec as vmec_module
 from simsopt import mhd
+
+
+def plot_surface(
+    surface: surface_rz_fourier.SurfaceRZFourier,
+    n_theta: int = 50,
+    n_phi: int = 51,
+    include_endpoints: bool = True,
+) -> go.Figure:
+    """Plot a continuous surface in 3D space using Plotly.
+
+    Args:
+        surface: The surface to plot.
+        n_theta: Number of samples in the theta angle.
+        n_phi: Number of samples in the phi angle.
+        include_endpoints: Whether to include the last point both poloidally and
+            toroidally.
+
+    Returns:
+        The figure with the surface added.
+    """
+    fig = go.Figure()
+
+    theta_phi = surface_utils.make_theta_phi_grid(
+        n_theta, n_phi, include_endpoints=include_endpoints
+    )
+    points = surface_rz_fourier.evaluate_points_xyz(surface, theta_phi)
+
+    # Ensure points is a NumPy array with shape (n_phi, n_theta, 3)
+    points = np.array(points)
+    x = points[..., 0]
+    y = points[..., 1]
+    z = points[..., 2]
+
+    fig.add_trace(go.Surface(x=x, y=y, z=z))
+
+    default_layout_kwargs = dict(
+        height=600,
+        width=600,
+        xaxis_title="R",
+        yaxis_title="Z",
+        xaxis=dict(showgrid=False),
+        yaxis=dict(showgrid=False),
+        plot_bgcolor="rgba(0, 0, 0, 0)",
+    )
+
+    fig.update_layout(
+        default_layout_kwargs,
+        scene=dict(
+            aspectmode="data",  # maintains the true aspect ratio
+            xaxis=dict(title="X"),
+            yaxis=dict(title="Y"),
+            zaxis=dict(title="Z"),
+        ),
+    )
+
+    return fig
 
 
 def plot_boozer_surfaces(
