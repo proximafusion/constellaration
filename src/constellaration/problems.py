778a3a7acc8729a7f2b96d3bb02547e6c017d322
Maria Pascu <mariap@proximafusion.com>
2025-05-05 17:51:07 +0200

    Add multi objective problem

--- src/constellaration/problems.py
+++ src/constellaration/problems.py
@@ -1,7 +1,9 @@
 import abc
 
+import jaxtyping as jt
 import numpy as np
 import pydantic
+from pymoo.indicators import hv
 from constellaration import forward_model
 from constellaration.geometry import surface_rz_fourier
 
@@ -20,6 +22,12 @@ class SingleObjectiveProblem(abc.ABC):
 
     @abc.abstractmethod
     def _score(self, metrics: forward_model.ConstellarationMetrics) -> float:
+        pass
+
+
+class MultiObjectiveProblem(abc.ABC):
+    @abc.abstractmethod
+    def score(self, boundaries: list[surface_rz_fourier.SurfaceRZFourier]) -> float:
         pass
 
 
@@ -105,6 +113,93 @@ class SimpleToBuildQIStellarator(SingleObjectiveProblem, pydantic.BaseModel):
         ):
             return False
         return True
+
+
+class MHDStableQIStellarator(MultiObjectiveProblem, pydantic.BaseModel):
+    _aspect_ratio_over_edge_rotational_transform_upper_bound: pydantic.PositiveFloat = (
+        10.0
+    )
+
+    _edge_rotational_transform_over_n_field_periods_lower_bound: (
+        pydantic.PositiveFloat
+    ) = 0.25
+
+    _log10_qi_upper_bound: pydantic.NegativeFloat = -3.5
+
+    _edge_magnetic_mirror_ratio_upper_bound: pydantic.PositiveFloat = 0.25
+
+    _flux_compression_in_regions_of_bad_curvature_upper_bound: (
+        pydantic.PositiveFloat
+    ) = 0.9
+
+    _vacuum_well_lower_bound: pydantic.NonNegativeFloat = 0.0
+
+    def score(self, boundaries: list[surface_rz_fourier.SurfaceRZFourier]) -> float:
+        feasible_metrics: list[forward_model.ConstellarationMetrics] = []
+        for boundary in boundaries:
+            setting = forward_model.ConstellarationSettings.default_high_fidelity()
+            metrics, _ = forward_model.forward_model(
+                boundary=boundary,
+                settings=setting,
+            )
+            if self.is_feasible(metrics=metrics):
+                feasible_metrics.append(metrics)
+        X = np.array(
+            [
+                (
+                    -1.0 * m.minimum_normalized_magnetic_gradient_scale_length,
+                    1.0 * m.aspect_ratio,
+                )
+                for m in feasible_metrics
+            ]
+        )
+        reference_point = np.array([-5.0, 1.0])
+        return _hypervolume(
+            X=X,
+            reference_point=reference_point,
+        )
+
+    def is_feasible(self, metrics: forward_model.ConstellarationMetrics) -> bool:
+        assert metrics.qi is not None
+        assert metrics.flux_compression_in_regions_of_bad_curvature is not None
+        if (
+            _is_constraint_violated(
+                value=metrics.aspect_ratio_over_edge_rotational_transform,
+                upper_bound=self._aspect_ratio_over_edge_rotational_transform_upper_bound,
+            )
+            or _is_constraint_violated(
+                value=metrics.edge_rotational_transform_over_n_field_periods,
+                lower_bound=self._edge_rotational_transform_over_n_field_periods_lower_bound,
+            )
+            or _is_constraint_violated(
+                value=np.log10(metrics.qi),
+                upper_bound=self._log10_qi_upper_bound,
+            )
+            or _is_constraint_violated(
+                value=metrics.edge_magnetic_mirror_ratio,
+                upper_bound=self._edge_magnetic_mirror_ratio_upper_bound,
+            )
+            or _is_constraint_violated(
+                value=metrics.flux_compression_in_regions_of_bad_curvature,
+                upper_bound=self._flux_compression_in_regions_of_bad_curvature_upper_bound,
+            )
+            or _is_constraint_violated(
+                value=metrics.vacuum_well,
+                lower_bound=self._vacuum_well_lower_bound,
+            )
+        ):
+            return False
+        return True
+
+
+def _hypervolume(
+    X: jt.Float[np.ndarray, "n_points n_metrics"],
+    reference_point: jt.Float[np.ndarray, " n_metrics"],
+) -> float:
+    indicator = hv.Hypervolume(ref_point=reference_point)
+    output = indicator(X)
+    assert output is not None
+    return output
 
 
 def _normalize_between_bounds(
