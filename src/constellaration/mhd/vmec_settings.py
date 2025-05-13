959ed46fab497c92ce2e08fe41a562544b38650f
Maria Pascu <mariap@proximafusion.com>
2025-05-13 11:59:38 +0200

    Ultra low fidelity vmec settings

--- src/constellaration/mhd/vmec_settings.py
+++ src/constellaration/mhd/vmec_settings.py
@@ -58,6 +58,7 @@ class VmecPresetSettings(pydantic.BaseModel):
         "from_boundary_resolution",
         "high_fidelity",
         "low_fidelity",
+        "ultra_low_fidelity",
     ] = "from_boundary_resolution"
     """Available presets:
 
@@ -68,13 +69,29 @@ class VmecPresetSettings(pydantic.BaseModel):
             convergence rate.
      * `low_fidelity`: Optimizes for runtime over correctness/fidelity and VMEC
             convergence rate.
+    * `ultra_low_fidelity`: Optimizes for runtime over correctness/fidelity and
+            VMEC convergence rate, but with a very low resolution. This is meant to be
+            used for very fast convergence in optimization tasks where.
 
     For details on the presets meaning, please refer to the respective factory functions
     in `mhd/vmec_settings_utils.py`.
 
     """
     verbose: bool = False
-    """Whether to print VMEC's output to stdout."""
+    """Whether to print VMEC++'s output to stdout."""
+
+    multithreaded: bool = False
+    """Whether to run VMEC++ in multithreaded mode, in which case it will spawn as many
+    threads as the number of logical CPU cores.
+
+    Note that if CPU cores are overcommitted (i.e. the cores are shared with other
+    running processes), performance could degrade significantly even with respect to
+    single-threaded runs: only turn this on if VMEC++ will be the only process running
+    on the machine.
+
+    Also note that multithreaded runs are not perfectly reproducible because of
+    concurrency and floating point arithmetic.
+    """
 
 
 def create_vmec_settings_from_preset(
@@ -104,10 +121,23 @@ def create_vmec_settings_from_preset(
         vmec_settings = vmec_settings_from_boundary_resolution(
             boundary=boundary,
         )
+    elif settings.fidelity == "ultra_low_fidelity":
+        vmec_settings = vmec_settings_ultra_low_fidelity_fixed_boundary(
+            boundary=boundary,
+        )
     else:
         raise ValueError(f"Unknown settings fidelity: {settings.fidelity}")
 
     vmec_settings.verbose = settings.verbose
+
+    if settings.multithreaded:
+        # this tells VMEC++ to use as many threads as there are logical CPU cores
+        # VMEC++ handles max_threads>num_cores gracefully,
+        # and 16 is a sensible default to avoid over-subscription.
+        vmec_settings.max_threads = 16
+    else:
+        vmec_settings.max_threads = 1
+
     return vmec_settings
 
 
@@ -204,6 +234,54 @@ def vmec_settings_low_fidelity_fixed_boundary(
             ),
         ],
         time_step=0.7,
+    )
+
+
+def vmec_settings_ultra_low_fidelity_fixed_boundary(
+    boundary: surface_rz_fourier.SurfaceRZFourier,
+) -> VmecSettings:
+    """A very low-fidelity VMEC configuration that provides defaults for the given
+    boundary by matching the boundary's Fourier resolution to VMEC++'s internal
+    resolution.
+
+    Used for very fast convergence in optimization tasks.
+    """
+    # The criterion we use below to set n_toroidal_grid_points breaks down for ntor == 0
+    assert boundary.max_toroidal_mode != 0, "axisymmetric boundaries are not supported"
+
+    (
+        largest_non_zero_poloidal_mode,
+        largest_non_zero_toroidal_mode,
+    ) = surface_rz_fourier.get_largest_non_zero_modes(surface=boundary)
+
+    # Based on past VMEC experience, this is a solution that should allow good
+    # convergence rates while still providing reasonable resolution
+    # until we have a more data-driven suggestion.
+    n_poloidal_modes = largest_non_zero_poloidal_mode + 2
+    max_toroidal_mode = largest_non_zero_toroidal_mode + 2
+
+    (
+        n_poloidal_points,
+        n_toroidal_points,
+    ) = surface_utils.n_poloidal_toroidal_points_to_satisfy_nyquist_criterion(
+        n_poloidal_modes=n_poloidal_modes,
+        max_toroidal_mode=max_toroidal_mode,
+    )
+
+    return VmecSettings(
+        n_poloidal_modes=n_poloidal_modes,
+        max_toroidal_mode=max_toroidal_mode,
+        n_poloidal_grid_points=n_poloidal_points,
+        n_toroidal_grid_points=n_toroidal_points,
+        multigrid_steps=[
+            VmecMultiGridStep(
+                n_flux_surfaces=25, force_tolerance=1e-17, n_max_iterations=2000
+            ),
+            VmecMultiGridStep(
+                n_flux_surfaces=51, force_tolerance=1e-9, n_max_iterations=20000
+            ),
+        ],
+        time_step=0.9,
     )
 
 
